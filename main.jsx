05cb6a0bc39539436e6edd1ec33a3de49d3a3ad1
printbhejo504 <printbhejo504@gmail.com>
2026-08-10 12:35:52 +0530

    feat: add browser converter tools launcher

--- main.jsx
+++ main.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
+import { Wrench } from "lucide-react";
 import App from "./App";
+import ConverterTools from "./ConverterTools";
 import "./styles.css";
 import "./ui-overrides.css";
+import "./converter-tools.css";
+
+function Root() {
+  const [converterOpen, setConverterOpen] = useState(false);
+  return <>
+    <App />
+    <button className="converter-fab" onClick={() => setConverterOpen(true)} aria-label="Open PrintBhejo tools"><Wrench size={18}/><span>Tools</span></button>
+    {converterOpen && <ConverterTools onClose={() => setConverterOpen(false)} />}
+  </>;
+}
 
 createRoot(document.getElementById("root")).render(
-  <React.StrictMode><App /></React.StrictMode>
+  <React.StrictMode><Root /></React.StrictMode>
 );
